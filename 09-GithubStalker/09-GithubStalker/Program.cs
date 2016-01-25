e55612c44d99d81cda194c7043df08a50da3f27c
Mike Fossel <mikefossel@gmail.com>
2016-01-25 15:48:56 -0800

    finished with extras

--- 09-GithubStalker/09-GithubStalker/Program.cs
+++ 09-GithubStalker/09-GithubStalker/Program.cs
@@ -10,48 +10,105 @@ using Newtonsoft.Json.Linq;
 
 
 
+
 namespace _09_GithubStalker
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a GitHub username");
-            string username = Console.ReadLine();
+           
+            string username = "";
 
-            WebClient wc = new WebClient();
+            while (true)
             {
-                wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            
+                try
+                {
+                    Console.WriteLine("Enter a GitHub username");
+                    username = Console.ReadLine();
 
-                string json = wc.DownloadString("https://api.github.com/users/" + username);
-                var o = JObject.Parse(json);
+                    WebClient wc = new WebClient();
+
+                        wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                        string json = wc.DownloadString("https://api.github.com/users/" + username);
+                        var o = JObject.Parse(json);
+
+                        Console.WriteLine("Name: " + o["name"].ToString());
+                        Console.WriteLine("URL: " + o["url"].ToString());
+                        Console.WriteLine("Number of Followers: " + o["followers"].ToString());
+                        Console.WriteLine("");
+                        Console.WriteLine("Number of Repositories: " + o["public_repos"].ToString());
 
 
-                Console.WriteLine("Name: " + o["name"].ToString());
-                Console.WriteLine("URL: " + o["url"].ToString());
-                Console.WriteLine("Number of Followers: " + o["followers"].ToString());
-                Console.WriteLine("");
-                Console.WriteLine("Number of Repositories: " + o["public_repos"].ToString());
+                    WebClient wcRepo = new WebClient();
+         
+                        wcRepo.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                        String jsonRepo = wcRepo.DownloadString("https://api.github.com/users/" + username + "/repos");
+                        var objectRepo = JArray.Parse(jsonRepo);
+
+                        foreach (var repo in objectRepo)
+                        {
+                            Console.WriteLine("------" + repo["name"] + " " + repo["stargazers_count"] + " stars, " + repo["watchers_count"] + " watchers");
+                        }
+       
+                    break;
+                }
+
+
+                catch
+                {
+                    Console.WriteLine("That is not a valid GitHub username. ");
+                    Console.WriteLine();
+                }
             }
 
 
-            WebClient wcRepo = new WebClient();
+
+           
+
+            while (true)
             {
-                wcRepo.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                
+                
 
-                String jsonRepo = wcRepo.DownloadString("https://api.github.com/users/" + username + "/repos");
-                var objectRepo = JArray.Parse(jsonRepo);
+                try
+                {
+                    Console.WriteLine("To view number of commits and issues, enter project name: ");
+                    string projectCommit = Console.ReadLine();
+                    WebClient commitRepo = new WebClient();
+                    {
+                        commitRepo.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                        String jsonCommit = commitRepo.DownloadString("https://api.github.com/repos/" + username + "/" + projectCommit + "/commits");
+                        var objectCommit = JArray.Parse(jsonCommit);
+                        Console.WriteLine("Number of commits: " + objectCommit.Count.ToString());
+                    }
 
-               foreach (var repo in objectRepo)
-               {
-                   Console.WriteLine("------" + repo["name"] + ", " +  repo["stargazers_count"] + " stars, " + repo["watchers_count"] + " watchers");
-               }
+                    WebClient issueRepo = new WebClient();
+                    {
+                        issueRepo.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                        String jsonIssue = issueRepo.DownloadString("https://api.github.com/repos/" + username + "/" + projectCommit + "/issues");
+                        var objectIssue = JArray.Parse(jsonIssue);
+                        Console.WriteLine("Number of issues: " + objectIssue.Count.ToString());
+                        Console.ReadLine();
+                        break;
+                    }
 
 
 
-          
-                Console.ReadLine();
+
+
+                   
+                }
+                catch
+                {
+                    Console.WriteLine("That is not a valid project name. ");
+
+                }
+                
             }
+            
+
         }
     }
 }
